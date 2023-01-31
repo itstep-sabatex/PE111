0d1d363883ef7e4def4cc610760e72f35a2df4e8
sabatex <serhiy.lakas@hotmail.com>
2023-01-31 21:26:28 +0200

    *

--- ADONet/Program.cs
+++ ADONet/Program.cs
@@ -1,36 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Data;
 
-Console.WriteLine("Hello, World!");
-
 string connectionString = "FileName=Demo.db";
-
-var connection = (new Microsoft.Data.Sqlite.SqliteConnection(connectionString)) as IDbConnection; 
-IDataAdapter adapter = null;
-
-//connection.Database.
 try
 {
+    IDbConnection connection = (new Microsoft.Data.Sqlite.SqliteConnection(connectionString)) as IDbConnection;
     connection.Open();
-    var cmd = connection.CreateCommand();
-    //cmd.CommandText = $"create table student (id int,name varchar(100))";
+    IDbCommand cmd = connection.CreateCommand();
+     //cmd.CommandText = $"create table student (id int,name varchar(100))";
+    //cmd.CommandText = $"insert into student (id,name) values (3,'Іван Мазепа')";
     //cmd.ExecuteNonQuery();
-    cmd = connection.CreateCommand();
-    cmd.CommandText = $"insert into student (id,name) values (2,'Петро Скунць')";
-    cmd.ExecuteNonQuery();
-    cmd = connection.CreateCommand();
+    
     cmd.CommandText = $"select * from student";
-    var items = cmd.ExecuteReader();
-    while (items.Read())
+    var records = cmd.ExecuteReader();
+    while (records.Read())
     {
-        var id = items["id"];
-        var name = items["name"];
-        Console.WriteLine($"{id} {name}");
+         var id = records["id"];
+         var name = records["name"];
+         Console.WriteLine($"{id} {name}");
 
     }
+        connection.Close();
+    //connection.Database.
 
 
-    connection.Close();
+
+    //cmd.CommandText = $"create table student (id int,name varchar(100))";
+    //cmd.ExecuteNonQuery();
+    //cmd = connection.CreateCommand();
+    //cmd.CommandText = $"insert into student (id,name) values (2,'Петро Скунць')";
+    //cmd.ExecuteNonQuery();
+    //cmd = connection.CreateCommand();
+    //cmd.CommandText = $"select * from student";
+    //var items = cmd.ExecuteReader();
+    //while (items.Read())
+    //{
+    //    var id = items["id"];
+    //    var name = items["name"];
+    //    Console.WriteLine($"{id} {name}");
+
+    //}
+
+
+    //connection.Close();
 }
 catch (Exception ex)
 {
