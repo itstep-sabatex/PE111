d21e16970754c76ab70c17dd4812bdeebef21d77
sabatex <serhiy.lakas@hotmail.com>
2023-01-31 20:32:16 +0200

    Add project files.

---
+++ ADONet/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+string connectionString = "FileName=Demo.db";
+
+var connection = (new Microsoft.Data.Sqlite.SqliteConnection(connectionString)) as IDbConnection; 
+IDataAdapter adapter = null;
+
+//connection.Database.
+try
+{
+    connection.Open();
+    var cmd = connection.CreateCommand();
+    //cmd.CommandText = $"create table student (id int,name varchar(100))";
+    //cmd.ExecuteNonQuery();
+    cmd = connection.CreateCommand();
+    cmd.CommandText = $"insert into student (id,name) values (2,'Петро Скунць')";
+    cmd.ExecuteNonQuery();
+    cmd = connection.CreateCommand();
+    cmd.CommandText = $"select * from student";
+    var items = cmd.ExecuteReader();
+    while (items.Read())
+    {
+        var id = items["id"];
+        var name = items["name"];
+        Console.WriteLine($"{id} {name}");
+
+    }
+
+
+    connection.Close();
+}
+catch (Exception ex)
+{
+
+}
