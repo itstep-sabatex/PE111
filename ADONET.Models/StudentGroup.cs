c667f96960efa8543b7980ccaf0e8433b13d50da
sabatex <serhiy.lakas@hotmail.com>
2023-02-09 19:14:33 +0200

    *

---
+++ ADONET.Models/StudentGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADONET.Models
+{
+    public class StudentGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Student> Students { get; set; }
+    }
+
+ 
+}
