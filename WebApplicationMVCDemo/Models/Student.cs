9432b1034e20c61d454817ec396015d9a99a6060
sabatex <serhiy.lakas@hotmail.com>
2023-06-20 09:28:02 +0300

    *

--- WebApplicationMVCDemo/Models/Student.cs
+++ WebApplicationMVCDemo/Models/Student.cs
@@ -1,9 +1,15 @@
-﻿namespace WebApplicationMVCDemo.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationMVCDemo.Models
 {
     public class Student
     {
         public int Id { get; set; }
+        [DisplayName("Ім'я")]
+        
         public string Name { get; set; }
+        [DisplayName("Прізвище")]
         public string Surname { get; set; }
      }
 }
