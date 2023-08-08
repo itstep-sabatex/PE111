ddf775d78515fdc0fd1f7c2225187d73fe674964
Микола Боголюбов <Bogo_ik62@student.itstep.org>
2023-08-08 20:43:22 +0300

    додано автоматичне створення адміністратора по замовчуванню

--- ASP.NET/WebApplicationDemo/Program.cs
+++ ASP.NET/WebApplicationDemo/Program.cs
@@ -15,24 +15,35 @@ namespace WebApplicationDemo
                 var services = serviceScope.ServiceProvider;
                 var RoleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
                 var UserManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                var  _userStore = services.GetRequiredService<IUserStore<IdentityUser>>();
+                var  _emailStore = services.GetRequiredService<IUserEmailStore<IdentityUser>>();
                 var adminRole = await RoleManager.FindByNameAsync("Administrator");
+                const string adminUser = "www@were.ty";
+                const string adminPass = "12345";
                 if (adminRole == null)
                 {
                     var adminR = await RoleManager.CreateAsync(new IdentityRole("Administrator"));
                     if (adminR.Succeeded == false) { throw new Exception("Error create role "); }
                     adminRole = await RoleManager.FindByNameAsync("Administrator");
                 }
-                var userAdmin = await UserManager.FindByNameAsync("www@were.ty");
+                var userAdmin = await UserManager.FindByNameAsync(adminUser);
                 if (userAdmin == null)
                 {
-                    //var user = new IdentityUser
-                    //{
-                    //    UserName= "www@were.ty",
-                        
+                    var user = Activator.CreateInstance<IdentityUser>();
+                    await _userStore.SetUserNameAsync(user, adminUser, CancellationToken.None);
+                    await _emailStore.SetEmailAsync(user, adminUser, CancellationToken.None);
+                    var result = await UserManager.CreateAsync(user, adminPass);
+                    if (result.Succeeded)
+                    {
+                    
+                        var userId = await UserManager.GetUserIdAsync(user);
+                        var code = await UserManager.GenerateEmailConfirmationTokenAsync(user);
+                        var resultConfirm = await UserManager.ConfirmEmailAsync(user, code);
+                        userAdmin = await UserManager.FindByNameAsync(adminUser);
 
-                    //};
-                    //await UserManager.CreateAsync(user);
-                    throw new Exception("Not find admin user");
+                    }
+                    else
+                    { throw new Exception(); }
                 }
 
                 if (!await UserManager.IsInRoleAsync(userAdmin, "Administrator"))
