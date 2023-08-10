c3942e824d94d8ccd9db2513f6c50d05fb4a5b05
sabatex <serhiy.lakas@hotmail.com>
2023-08-10 21:39:56 +0300

    add part localization

--- ASP.NET/WebApplicationDemo/Program.cs
+++ ASP.NET/WebApplicationDemo/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.Text.RegularExpressions;
 using WebApplicationDemo.Data;
 
 namespace WebApplicationDemo
@@ -70,9 +73,29 @@ namespace WebApplicationDemo
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            builder.Services.AddLocalization();
+            builder.Services.AddLocalization(options=>options.ResourcesPath="Resources");
             builder.Services.AddRazorPages().AddDataAnnotationsLocalization().AddViewLocalization();
             builder.Services.AddControllers();
+
+
+            builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("uk")
+    };
+
+        options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+        options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+    options.AddInitialRequestCultureProvider(new CustomRequestCultureProvider(async context =>
+    {
+            // My custom request culture logic
+            return await Task.FromResult(new ProviderCultureResult("en"));
+        }));
+});
 
             var app = builder.Build();
 
