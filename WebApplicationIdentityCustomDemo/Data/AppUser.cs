aa20cac225006300d335eca180a08d0ab640944e
sabatex <serhiy.lakas@hotmail.com>
2023-08-23 09:48:38 +0300

    додано проект Identity Custom

---
+++ WebApplicationIdentityCustomDemo/Data/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplicationIdentityCustomDemo.Data
+{
+    public class AppUser:IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; } //Full user name Імя Прізвище
+        [PersonalData]
+        public string IdCode { get; set; } // ДРФО або номер паспорта
+    }
+}
---
+++ WebApplicationIdentityCustomDemo/Data/Migrations/20230822172638_m0.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplicationIdentityCustomDemo.Data.Migrations
+{
+    public partial class m0 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IdCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "AspNetUsers");
+        }
+    }
+}
