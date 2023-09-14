f0ccb4c7a3f26c1a953f34ea335e1342bf6756cb
sabatex <serhiy.lakas@hotmail.com>
2023-09-14 10:45:00 +0300

    *

--- ASP.NET/WebApplicationApiDemo/Program.cs
+++ ASP.NET/WebApplicationApiDemo/Program.cs
@@ -24,13 +24,15 @@ namespace WebApplicationApiDemo
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            //if (app.Environment.IsDevelopment())
+            //{
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+            //}
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
